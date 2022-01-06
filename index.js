5b17b1767b5d516a34c0a724a909b508d383aae9
gk-acaro <44534212+gk-acaro@users.noreply.github.com>
2022-01-06 13:21:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+function App(){
+  const [todos, setTodos] = React.useState([
+    {
+      text: 'learn react',
+      isCompleted: false,
+    },
+    {
+      text: 'meet friend for lunch',
+      isCompleted: false,
+    },
+    {
+      text: 'build todo app',
+      isCompleted: false,
+    }        
+  ]);
+
+  const [value, setValue] = React.useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, {text:value, isCompleted:false}];
+    setTodos(newTodos);
+    setValue('');
+  }
+  
+  return(
+    <>
+      {todos.map((todo, i) => (
+        <div className="todo" key={i}>{todo.text}</div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input 
+        type="text"
+        className="input"
+        value={value}
+        placeholder="Add ToDo ..."
+        onChange={e => setValue(e.target.value)}
+          />
+      </form>
+
+
+
+    </>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
